a6453014cbd8b9f15f8076df9878c001ab9c873a
Ollie <102157897+BOYOUNG-KANG@users.noreply.github.com>
2024-08-13 00:01:01 +0900

    [level 2] Title: 소수 찾기, Time: 43.58 ms, Memory: 81.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -0,0 +1,55 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+    public static boolean isPrime(int number) {
+        int temp = (int) Math.sqrt(number);
+        if (number <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= temp; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void dfs(String numbers, String s, int depth, Set<Integer> nums, boolean[] isVisited) {
+        if (depth > numbers.length()) {
+            return;
+        }
+
+        for (int i = 0; i < numbers.length(); i++) {
+            if(!isVisited[i]) {
+                isVisited[i] = true;
+                nums.add(Integer.parseInt(s + numbers.charAt(i)));
+                dfs(numbers ,s + numbers.charAt(i), depth + 1, nums, isVisited);
+                isVisited[i] = false;
+            }
+        }
+    }
+    
+	public static int solution(String numbers) {
+        int answer = 0;
+        Set<Integer> nums = new HashSet<>();
+        // 소수 : 0~거듭제곱까지 나누었을 모든 경우에 나머지가 0이 아님을 확인
+        // 순열
+        char[] temp = numbers.toCharArray();
+        int[] arr = new int[temp.length];
+        boolean[] isVisited = new boolean[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Character.getNumericValue(temp[i]);
+        }
+        
+        dfs(numbers, "", 0, nums, isVisited);
+        
+        for (int n : nums) {
+            if (isPrime(n)) {
+                System.out.println(n);
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
