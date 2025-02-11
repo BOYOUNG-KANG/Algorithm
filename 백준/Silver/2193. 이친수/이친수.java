103706d3bd52ea9098829f726db10a26c20a99bc
Ollie <102157897+BOYOUNG-KANG@users.noreply.github.com>
2025-02-11 21:39:09 +0900

    [Silver III] Title: 이친수, Time: 92 ms, Memory: 12872 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        long[] dp = new long[n+1];
+        if (n == 1) {
+            System.out.println(1);
+            return;
+        }
+
+        dp[1] = 1;
+        dp[2] = 1;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        System.out.println(dp[n]);
+    }
+}
