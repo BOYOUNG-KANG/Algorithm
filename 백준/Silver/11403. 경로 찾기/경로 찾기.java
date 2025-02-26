9c59cad5ecaea9f4147b7b89def6f4a8afaa989a
Ollie <102157897+BOYOUNG-KANG@users.noreply.github.com>
2025-02-26 23:47:39 +0900

    [Silver I] Title: 경로 찾기, Time: 256 ms, Memory: 18160 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.java
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -2,58 +2,41 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static boolean[][] visited;
-    static int[][] map;
     static int n;
+    static int[][] map;
+    static boolean[][] visited;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
         map = new int[n][n];
         visited = new boolean[n][n];
-        for(int i = 0; i < n; i++) {
+
+        for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0; j < n; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
-                if (map[i][j] == 1) visited[i][j] = true;
             }
         }
 
-        for(int i = 0; i < n; i++) {
-            bfs(i);
+        for (int i = 0; i < n; i++) {
+            dfs(i, i);
         }
 
-        for(int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 System.out.print(map[i][j]+" ");
             }
             System.out.println();
         }
+
     }
-
-    public static void bfs(int start) {
-        Queue<Integer> q = new LinkedList<>();
-        for (int j = 0; j < n; j++) {
-            if (map[start][j] == 1) {
-                q.add(j);
-            }
-        }
-
-        while (!q.isEmpty()) {
-            int i = q.poll();
-            boolean full = true;
-            for (int t = 0; t < n; t++) {
-                if (!visited[start][t])  full = false;
-            }
-            if (full == true) {
-                return;
-            }
-
-            for (int j = 0; j < n; j++) {
-                if (map[i][j] == 1 && map[start][j] == 0 && !visited[start][j]) {
-                    map[start][j] = 1;
-                    visited[start][j] = true;
-                    q.add(j);
-                }
+    private static void dfs(int start, int now) {
+        for (int next = 0; next < n; next++) {
+            if (map[now][next] == 1 && !visited[start][next]) {
+                map[start][next] = 1;
+                visited[start][next] = true;
+                dfs(start, next);
             }
         }
     }
