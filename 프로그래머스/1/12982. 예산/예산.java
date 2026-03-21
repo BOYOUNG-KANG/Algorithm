8c3d989bb36665df51d4c239b021cbcce8d72b8b
Ollie <102157897+BOYOUNG-KANG@users.noreply.github.com>
2026-03-21 11:47:16 +0900

    [level 1] Title: 예산, Time: 0.56 ms, Memory: 70.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        Arrays.sort(d);
+        int sum = 0;
+        for (int ele : d) {
+            if (sum + ele <= budget) {
+                sum += ele;
+                answer++;
+            }
+        }
+        return answer;
+    }
+    
+}
